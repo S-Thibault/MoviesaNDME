c3ef307c784c4559ab2fa7d8a09baaf61f34f9d2
S-Thibault <git config --global user.email sylvainvegeta@hotmail.fr>
2020-10-05 15:50:32 +0200

    first commit

---
+++ API/TMDBApi.js
@@ -0,0 +1,13 @@
+const API_TOKEN = "ea53b09b354e79aebad0de12fc432297"
+
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
